3e774b340b95e30da364bc1a493bdf6f2e190617
Matilda <matilda081133@gmail.com>
2026-06-25 10:48:15 +0300

    feat: make static export portable for arbitrary subdirectories

--- src/router.tsx
+++ src/router.tsx
@@ -1,15 +1,19 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createMemoryHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
+  const memoryHistory = createMemoryHistory({
+    initialEntries: ['/']
+  });
 
   const router = createRouter({
     routeTree,
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    history: typeof window !== 'undefined' ? memoryHistory : undefined,
   });
 
   return router;
